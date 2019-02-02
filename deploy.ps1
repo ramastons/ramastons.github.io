cfd96a92a47e3df01c7c6c44148e9a2bfe8c73c6
Joan Jané <jjaneballester@gmail.com>
2019-02-02 02:08:17 +0100

    Added deploy script

---
+++ deploy.ps1
@@ -0,0 +1,21 @@
+# Build site
+bundle exec jekyll build
+
+# Clean existing files and copy artifact
+rmdir ../_site -recurse -force -Confirm:$false -ErrorAction:SilentlyContinue
+Copy-Item "./_site" -Destination "../_site" -Recurse
+cd ../_site
+
+# Init repo for gh-pages
+git init
+git checkout -b gh-pages
+git add *
+git commit -m "gh-pages deployment"
+
+# Upload site to github
+git remote add origin https://github.com/ramastons/web.git
+git push origin gh-pages:gh-pages
+
+# Return to path and clean up
+cd ../www.ramastons.com
+rmdir ../_site -recurse -force -Confirm:$false -ErrorAction:SilentlyContinue
