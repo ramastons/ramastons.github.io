1b40b00e9f5e8328674dad705c6d9e58b46680f0
Joan Jané <jjaneballester@gmail.com>
2019-02-10 01:05:53 +0100

    Fix utf-8 encoding on css. Skip next slide on carousel if changed manually

--- js/common.js
+++ js/common.js
@@ -1,14 +1,20 @@
 function initCarousel() {
     var slideShowSeconds = 5;
-    
+    var lastSlide = null;
     setInterval(function() {
         var slides = document.querySelectorAll('.js-carousel-option').length;
         var currentSlide = getCurrentSlide();
+        if (lastSlide != null && lastSlide != currentSlide) {
+            lastSlide = currentSlide;
+            return;
+        }
         if (currentSlide >= slides) {
             currentSlide = 1;
         } else {
             currentSlide++;
         }
+        
+        lastSlide = currentSlide;
         document.querySelector('.js-carousel-option[value="' + currentSlide + '"]').checked = true
     }, slideShowSeconds * 1000);
 }
