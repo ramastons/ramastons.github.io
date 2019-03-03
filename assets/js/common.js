b115ff233892328d8067527161ca87ef5302761a
Joan Jané <--global>
2019-03-03 01:47:06 +0100

    Add carousel auto height feature

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,34 +1,75 @@
 function initCarousel(element, slideShowSeconds) {
-  if (!element) {
-    return;
-  }
-  var lastSlide = null;
+  this.element = element;
+  this.lastSlide = null;
+  this.events = [];
 
   setInterval(function () {
-    var slides = element.querySelectorAll('.js-carousel-option').length;
-    var currentSlide = getCurrentSlide(element);
-    if (lastSlide != null && lastSlide != currentSlide) {
-      lastSlide = currentSlide;
+    if (this.element.classList.contains("js-stopLoop")) {
       return;
     }
-    if (currentSlide >= slides) {
-      currentSlide = 1;
-    } else {
-      currentSlide++;
-    }
-
-    lastSlide = currentSlide;
-    element.querySelector('.js-carousel-option[value="' + currentSlide + '"]').checked = true
+    loopSlides();
   }, slideShowSeconds * 1000);
 
-  function getCurrentSlide(element) {
+  this.events.push(window.addEventListener('resize', function() {
+    setHeightAuto();
+  }));
+
+  this.element.querySelectorAll('.js-carousel-option').forEach(function (option) {
+    this.events.push(option.addEventListener('change', function() {
+      setHeightAuto();
+    }));
+  });
+
+  setHeightAuto();
+
+  function loopSlides() {
+    var slidesCount = getSlidesCount();
+    var currentSlide = getCurrentSlide(element);
+    if (this.lastSlide != null && this.lastSlide != currentSlide) {
+      // add an additional loop when slide is changed manually
+      this.lastSlide = currentSlide;
+      return;
+    }
+
+    currentSlide = currentSlide >= slidesCount ? 1 : currentSlide + 1;
+    this.lastSlide = currentSlide;
+
+    displaySlide(currentSlide);
+    setImageHeight(currentSlide);
+  }
+
+  function getSlidesCount() {
+    return this.element.querySelectorAll('.js-carousel-option').length;
+  }
+
+  function displaySlide(currentSlide) {
+    this.element.querySelector('.js-carousel-option[value="' + currentSlide + '"]').checked = true;
+  }
+
+  function getCurrentSlide() {
     var currentSlide = 1;
-    element.querySelectorAll('.js-carousel-option').forEach(function (i) {
+    this.element.querySelectorAll('.js-carousel-option').forEach(function (i) {
       if (i.checked) {
         currentSlide = i.value;
       }
     });
     return parseInt(currentSlide);
+  }
+
+  function setImageHeight(index) {
+    var slide = this.element.querySelector('.js-slideImage[data-image-index="'+index+'"]');
+    if (slide.complete && slide.clientHeight > 0) {
+      this.element.parentElement.style.height = slide.clientHeight + "px";
+      return;
+    }
+
+    slide.addEventListener('load', function() {
+      setImageHeight(index);
+    });
+  }
+
+  function setHeightAuto() {
+    setImageHeight(getCurrentSlide());
   }
 }
 
